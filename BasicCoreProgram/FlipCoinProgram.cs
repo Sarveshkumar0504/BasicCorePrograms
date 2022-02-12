aea6755514860bdc14b7c88a4a23270ecfd06387
sarvesh <sarveshkumar452000@gmail.com>
2022-02-11 21:14:47 -0800

    FlipCoin program added Successfully

---
+++ BasicCoreProgram/FlipCoinProgram.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class FlipCoinProgram1
+    {
+        public int HeadCount = 0;
+        public int TailCount = 0;
+        public int HeadPercentage = 0;
+        public int TailPercentage = 0;
+        Random random = new Random();
+        public void PercentageCheck()
+        {
+            Console.WriteLine("Enter the number of times you want to flip the coin");
+            int CoinFlip = Convert.ToInt32(Console.ReadLine());
+
+            
+            for (int i = 0; i < CoinFlip; i++)
+            {
+                double CheckFlip = random.Next(0, 2);
+                if (CheckFlip < 0.5)
+                {
+                    Console.WriteLine("This is Head");
+                    HeadCount++;
+                }
+                else
+                {
+                    Console.WriteLine("This is Tail");
+                    TailCount++;
+                }
+            }
+            
+            HeadPercentage = HeadCount * (100 / CoinFlip);
+            TailPercentage = TailCount * (100 / CoinFlip);
+            Console.WriteLine("Percentage of head :" + HeadPercentage);
+            Console.WriteLine("Percentage of head :" + TailPercentage);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ BasicCoreProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class FlipCoinProgram
+    {
+        static void Main(string[] args )
+        {
+            Console.WriteLine("Welcome to Basic Core Program");
+            FlipCoinProgram1 program = new FlipCoinProgram1();
+            program.PercentageCheck();
+            Console.ReadLine();
+        }
+    }
+}
